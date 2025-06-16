7b47d6898205f5a81e69e0f71f17ec4036579571
Dave Franco <self@daveops.sh>
2025-06-16 22:20:48 +0200

    infra changes
    - ecr repo
    - secrets
    - vpc

---
+++ .infra/main.tf
@@ -0,0 +1,88 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-2"
+  profile = "personal"
+}
+
+## ECR 
+resource "aws_ecr_repository" "this" {
+  name = "davebot"
+  
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_lifecycle_policy" "this" {
+  repository = aws_ecr_repository.this.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Expire images older than 1 days",
+            "selection": {
+                "tagStatus": "untagged",
+                "countType": "sinceImagePushed",
+                "countUnit": "days",
+                "countNumber": 1
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+
+## Secret Manager
+resource "aws_secretsmanager_secret" "this" {
+  name                    = "davebot"
+  recovery_window_in_days = 0
+}
+
+resource "aws_secretsmanager_secret_version" "this" {
+  secret_id = aws_secretsmanager_secret.this.id
+  secret_string = jsonencode({
+    OPENAI_KEY = "",
+    DB_STRING  = ""
+    }
+  )
+  lifecycle {
+    ignore_changes = [secret_string]
+  }
+}
+
+## VPC 
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "davebot-net"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "demo"
+  }
+}
+
+## RDS 
+## ECS 
