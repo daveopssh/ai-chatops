dca7089f354239efabc28a108a881e41b5d8884b
Dave Franco <self@daveops.sh>
2025-06-16 23:03:34 +0200

    rds ready

--- .infra/main.tf
+++ .infra/main.tf
@@ -10,15 +10,27 @@ terraform {
 provider "aws" {
   region  = "us-east-2"
   profile = "personal"
+  default_tags {
+    tags = local.tags
+  }
+}
+
+locals {
+  tags = {
+    Terraform   = "true"
+    Environment = "demo"
+  }
+
 }
 
 ## ECR 
 resource "aws_ecr_repository" "this" {
   name = "davebot"
-  
+
   image_scanning_configuration {
     scan_on_push = true
   }
+
 }
 
 resource "aws_ecr_lifecycle_policy" "this" {
@@ -44,7 +56,6 @@ resource "aws_ecr_lifecycle_policy" "this" {
 }
 EOF
 }
-
 
 ## Secret Manager
 resource "aws_secretsmanager_secret" "this" {
@@ -77,12 +88,58 @@ module "vpc" {
 
   enable_nat_gateway = true
   single_nat_gateway = true
+}
 
-  tags = {
-    Terraform   = "true"
-    Environment = "demo"
+resource "aws_security_group" "davebot_db_sg" {
+  name        = "davebot-db-sg"
+  description = "Security group for DaveBot RDS access"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["213.32.243.91/32"] # Adjust this to restrict access
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 ## RDS 
+module "db" {
+  source = "terraform-aws-modules/rds/aws"
+
+  identifier = "davebotdb"
+
+  engine            = "postgres"
+  engine_version    = "17.5"
+  instance_class    = "db.t4g.micro"
+  allocated_storage = 10
+
+  db_name  = "vector_db"
+  username = "davebotAdmin"
+  port     = "5432"
+
+  iam_database_authentication_enabled = false
+
+  vpc_security_group_ids = [aws_security_group.davebot_db_sg.id]
+
+  # DB subnet group
+  create_db_subnet_group          = true
+  db_subnet_group_use_name_prefix = true
+  subnet_ids                      = module.vpc.public_subnets
+  publicly_accessible             = true
+  # DB parameter group
+  create_db_parameter_group = false
+
+  # Database Deletion Protection
+  deletion_protection = false
+}
+
+
 ## ECS 
