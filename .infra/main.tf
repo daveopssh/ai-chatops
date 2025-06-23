f55f5e83c43137cbfd09e2779f0b757f554b6d57
Dave Franco <self@daveops.sh>
2025-06-23 23:11:17 +0200

    infra, still need to debug

--- .infra/main.tf
+++ .infra/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
   }
 }
@@ -16,6 +16,7 @@ provider "aws" {
 }
 
 locals {
+  myip = "213.32.243.91/32"
   tags = {
     Terraform   = "true"
     Environment = "demo"
@@ -67,7 +68,6 @@ resource "aws_secretsmanager_secret_version" "this" {
   secret_id = aws_secretsmanager_secret.this.id
   secret_string = jsonencode({
     OPENAI_KEY = "",
-    DB_STRING  = ""
     }
   )
   lifecycle {
@@ -99,7 +99,7 @@ resource "aws_security_group" "davebot_db_sg" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["213.32.243.91/32"] # Adjust this to restrict access
+    cidr_blocks = [local.myip] # Adjust this to restrict access
   }
 
   egress {
@@ -142,4 +142,230 @@ module "db" {
 }
 
 
-## ECS 
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+data "aws_iam_policy_document" "ecs_task_execution_role_policy" {
+  statement {
+    actions = [
+      "secretsmanager:*",
+      #"secretmanager:GetSecretValue",
+      #"secretmanager:DescribeSecret",
+    ]
+
+    resources = [
+      aws_secretsmanager_secret.this.arn,
+      module.db.db_instance_master_user_secret_arn
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "ecs_custom_task_execution_role_policy" {
+  name   = "EcsDaveBotTaskExecutionRolePolicy"
+  role   = aws_iam_role.ecs_task_execution_role.id
+  policy = data.aws_iam_policy_document.ecs_task_execution_role_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "davebot-alb"
+  vpc_id  = module.vpc.vpc_id
+  subnets = module.vpc.public_subnets
+
+  # Security Group
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      description = "HTTP web traffic"
+      cidr_ipv4   = local.myip
+    }
+  }
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "10.0.0.0/16"
+    }
+  }
+
+  listeners = {
+    http = {
+      port     = 80
+      protocol = "HTTP"
+      forward = {
+        target_group_key = "ecs"
+      }
+    }
+  }
+
+  target_groups = {
+    ecs = {
+      name_prefix       = "ia"
+      protocol          = "HTTP"
+      port              = 8000
+      target_type       = "ip"
+      create_attachment = false
+    }
+  }
+
+  tags = local.tags
+}
+
+resource "aws_security_group" "davebot_ecs_sg" {
+  name        = "davebot-ecs-sg"
+  description = "Security group for DaveBot ECS service"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [module.alb.security_group_id] # Adjust this to restrict access
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_cluster" "davebot_cluster" {
+  name = "davebot-cluster"
+
+  tags = local.tags
+}
+
+resource "aws_cloudwatch_log_group" "davebot" {
+  name              = "/ecs/davebot"
+  retention_in_days = 7
+}
+
+resource "aws_ecs_task_definition" "davebot_task" {
+  family                   = "davebot-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "davebot-container"
+      image     = "${aws_ecr_repository.this.repository_url}:amd64"
+      cpu       = 256
+      memory    = 512
+      essential = true
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = "/ecs/davebot"
+          awslogs-region        = "us-east-2"
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+      
+      portMappings = [
+        {
+          containerPort = 8000
+          hostPort      = 8000
+          protocol      = "tcp"
+        }
+      ]
+      
+      secrets = [
+        {
+          name  = "OPENAI_KEY"
+          valueFrom = aws_secretsmanager_secret.this.arn
+        },
+        {
+          name  = "DB_USER"
+          valueFrom = "${module.db.db_instance_master_user_secret_arn}:username::"
+        },
+        {
+          name  = "DB_PASSWORD"
+          valueFrom = "${module.db.db_instance_master_user_secret_arn}:password::"
+        }
+      ]
+      environment = [
+        {
+          name  = "DB_HOST"
+          value = module.db.db_instance_address
+        }
+      ]
+    }
+  ])
+}
+
+
+resource "aws_ecs_service" "davebot_service" {
+  name            = "davebot-ecs-service"
+  cluster         = aws_ecs_cluster.davebot_cluster.id
+  task_definition = aws_ecs_task_definition.davebot_task.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = module.vpc.private_subnets
+    security_groups  = [aws_security_group.davebot_ecs_sg.id]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = module.alb.target_groups["ecs"].arn
+    container_name   = "davebot-container"
+    container_port   = 8000
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.ecs_task_execution_role_policy_attachment,
+    aws_ecr_repository.this,
+    aws_secretsmanager_secret.this,
+    module.db,
+    module.vpc,
+    module.alb
+  ]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
